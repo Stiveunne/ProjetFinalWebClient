01aa06e78926472d8613d0bfa4815ae83867b433
Matheo821 <loirat2005@gmail.com>
2025-01-20 18:08:55 +0100

    Delete maquetteWEB directory

--- maquetteWEB/assets/js/sliderPartage.js
+++
@@ -1,5 +0,0 @@
-function showSlide(index) {
-    const slider = document.querySelector('.slider');
-    const slideWidth = slider.children[0].clientWidth;
-    slider.style.transform = `translateX(-${index * slideWidth}px)`;
-}
