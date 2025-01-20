55af460cee14541110fb0bed4e12ce9dfdb3bb22
Matheo821 <loirat2005@gmail.com>
2025-01-20 20:33:09 +0100

    Ajout image et bouton
    Il faudra terminer le css du texte ainsi que rajouter un lien sur le bouton

--- assets/js/sliderPartage.js
+++ assets/js/sliderPartage.js
@@ -2,4 +2,9 @@ function showSlide(index) {
     const slider = document.querySelector('.slider');
     const slideWidth = slider.children[0].clientWidth;
     slider.style.transform = `translateX(-${index * slideWidth}px)`;
+
+    // Gestion de l'état des boutons
+    const buttons = document.querySelectorAll('.buttons button');
+    buttons.forEach(button => button.classList.remove('active'));
+    buttons[index].classList.add('active');
 }
