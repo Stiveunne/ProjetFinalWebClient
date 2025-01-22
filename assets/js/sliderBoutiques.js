75ec6eccc34527b1ed731689f044f688a55b7460
Matheo821 <loirat2005@gmail.com>
2025-01-22 16:53:29 +0100

    Final Version
    Ajout des commentaires définitifs
    Changement possible de la couleur des flèches

--- assets/js/sliderBoutiques.js
+++ assets/js/sliderBoutiques.js
@@ -3,32 +3,33 @@ const nextButton = document.querySelector('.next-button');
 const sliderList = document.querySelector('.list');
 const items = Array.from(sliderList.children);
 const totalItems = items.length;
-let currentIndex = 0; // Position actuelle du slider
+// Position actuelle
+let currentIndex = 0; 
 
-// Fonction pour ajuster les positions des images
+// Update les positions des images
 function updatePositions() {
     items.forEach((item, index) => {
-        const position = (index + currentIndex) % totalItems; // Calcul circulaire pour positionner les images
-        item.style.setProperty('--position', position + 1); // Mise à jour de la position avec CSS variable
+        const position = (index + currentIndex) % totalItems;
+        item.style.setProperty('--position', position + 1); 
     });
 }
 
-// Fonction pour passer à l'image suivante
+// Permet de passer à l'image suivante
 function nextSlide() {
-    currentIndex = (currentIndex + 1) % totalItems; // Défilement circulaire
+    currentIndex = (currentIndex + 1) % totalItems;
     updatePositions();
 }
 
-// Fonction pour revenir à l'image précédente
+// Permet de revenir à l'image précédente
 function prevSlide() {
-    currentIndex = (currentIndex - 1 + totalItems) % totalItems; // Défilement circulaire
+    currentIndex = (currentIndex - 1 + totalItems) % totalItems; 
     updatePositions();
 }
 
-// Ajouter des événements de clic pour les flèches
+// Event au click
 nextButton.addEventListener('click', nextSlide);
 prevButton.addEventListener('click', prevSlide);
 
-// Initialisation des positions au début
+// Positions de départ
 updatePositions();
 
